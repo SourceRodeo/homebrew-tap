a30a3ea6fd874d6f4935dd875690db88009fe0ef
Geordie Kaytes <david.kaytes@gmail.com>
2026-05-09 08:28:35 -0400

    Update Formula/special.rb for v0.10.0

--- Formula/special.rb
+++ Formula/special.rb
@@ -4,20 +4,20 @@
 class Special < Formula
   desc "Repo-native semantic spec tool"
   homepage "https://github.com/sourcerodeo/special"
-  version "0.9.1"
+  version "0.10.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.9.1/special-cli-aarch64-apple-darwin.tar.xz"
-    sha256 "a5d67f44d2476e9702485dcf9eea9d38bec06984965438372e35db782c954acb"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.10.0/special-cli-aarch64-apple-darwin.tar.xz"
+    sha256 "c8469d900ff07e84003d226dbb95b11721a43bc79a1bf501f6eb79bcd45e49ec"
   elsif OS.mac?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.9.1/special-cli-x86_64-apple-darwin.tar.xz"
-    sha256 "7cab660e0289a8ff80820d2ce9a6aa3f4f5940827f35f18591d5505cb7ed70c7"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.10.0/special-cli-x86_64-apple-darwin.tar.xz"
+    sha256 "9df4bd52eb142b5430ea0c51f1e7425d855624f19c7292640c5d378a35dea6af"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.9.1/special-cli-aarch64-unknown-linux-gnu.tar.xz"
-    sha256 "841e56134cec1ec0ae6776d01179c59d79d070169c27ef5fd03f231e5a3b8f46"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.10.0/special-cli-aarch64-unknown-linux-gnu.tar.xz"
+    sha256 "7a43a8c328ae472821dd69f14bfe1996cf4b0177f330507c023333074eab969a"
   elsif OS.linux?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.9.1/special-cli-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "b09b2469f0148e36557c74e7470f184e6d87ac57fb81e815324167b8dbc8b244"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.10.0/special-cli-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "b1e28a64d64cd54a2e37bad4c41711e3fa57d848c2481c32bb7948ad47e28f37"
   end
 
   def install
