7a035dbe8edd004028c8cc5535883e2f22cc3af0
Geordie Kaytes <david.kaytes@gmail.com>
2026-05-26 22:48:46 -0400

    Update Formula/special.rb for v0.11.1

--- Formula/special.rb
+++ Formula/special.rb
@@ -4,20 +4,20 @@
 class Special < Formula
   desc "Repo-native semantic spec tool"
   homepage "https://github.com/sourcerodeo/special"
-  version "0.11.0"
+  version "0.11.1"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.11.0/special-cli-aarch64-apple-darwin.tar.xz"
-    sha256 "0743de2ce57fdba823be3a8c77377db60aa1b3811a1a22c58195eee0dceb45ef"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.11.1/special-cli-aarch64-apple-darwin.tar.xz"
+    sha256 "f2e5477e290ff735d71a800be75f8b51e0c9b6a6f04d4fe8856adc81cd80002b"
   elsif OS.mac?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.11.0/special-cli-x86_64-apple-darwin.tar.xz"
-    sha256 "56de0a170779bc186ce3cd1d283130ac30d311ccda9af15b20bd1bb8afe155cb"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.11.1/special-cli-x86_64-apple-darwin.tar.xz"
+    sha256 "6e1c1fe85691d0d7fc929a41947b83a089d195df40eefd7ad3c36946a763a7cc"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.11.0/special-cli-aarch64-unknown-linux-gnu.tar.xz"
-    sha256 "5a953d76a41963519713936913ceb161e2e199485041ccc70f1356bc1ec2bfa9"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.11.1/special-cli-aarch64-unknown-linux-gnu.tar.xz"
+    sha256 "1803aef1fc9efd93fc76ece205accfe7e739ad3b6449e695aca6b9c267bd5d28"
   elsif OS.linux?
-    url "https://github.com/sourcerodeo/special/releases/download/v0.11.0/special-cli-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "32779aaa709b0d6758a276b8bce0e794a38d2efb3308e1673c9584f33e175300"
+    url "https://github.com/sourcerodeo/special/releases/download/v0.11.1/special-cli-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "507181d37c32a6c68bff1292312e10144b524080c5ecd4400c01a2099fecfa16"
   end
 
   def install
